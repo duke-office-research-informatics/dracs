6e912311f76a85cc743677ecedf4364b0c29475b
Byron Wall <byron.wall@duke.edu>
2019-05-21 09:20:58 -0400

    add IconInfo to icons

---
+++ src/icons/info/info.js
@@ -0,0 +1,36 @@
+import React from "react";
+import propTypes from "prop-types";
+import Icon from "../icon_wrapper.js";
+
+const IconInfo = props => {
+  return (
+    <Icon
+      className={props.className}
+      viewBox="0 0 24 24"
+      width={props.size}
+      height={props.size}
+      color={props.color}
+      hoverColor={props.hoverColor}
+      role="img"
+      aria-label="info"
+    >
+      <title>info</title>
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
+    </Icon>
+  );
+};
+
+IconInfo.defaultProps = {
+  size: 24,
+  color: "#666",
+};
+
+IconInfo.propTypes = {
+  className: propTypes.string,
+  size: propTypes.number,
+  color: propTypes.oneOfType([propTypes.string, propTypes.func]),
+  hoverColor: propTypes.oneOfType([propTypes.string, propTypes.func]),
+};
+
+export default IconInfo;
