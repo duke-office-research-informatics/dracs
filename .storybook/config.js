ddf0387ea7e7787fe703c2f8582951126e3e97d7
Byron Wall <byron.wall@duke.edu>
2019-07-18 11:26:11 -0400

    newest version of 'babel-plugin-styled-components breaks build/tests, reverting to version 1.10.0 until there is time to update dracs/s-c versions across apps

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
-import "@babel/polyfill";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import React from "react";
 import styled from "styled-components";
 import { configure, addDecorator } from "@storybook/react";
--- config/jestEnzymeConfig.js
+++ config/jestEnzymeConfig.js
@@ -3,7 +3,8 @@ import Adapter from "enzyme-adapter-react-15";
 import { createSerializer } from "enzyme-to-json";
 import "jest-styled-components";
 import { toHaveNoViolations } from "jest-axe";
-import "@babel/polyfill";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
