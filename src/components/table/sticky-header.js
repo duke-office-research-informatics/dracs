2974f104df67a64bb1815dff17d763a32ebf01c8
Byron Wall <byron.wall@duke.edu>
2020-01-07 09:40:44 -0500

    continue troubleshooting ref forwarding for persistent headers/columns in table components

--- src/components/table/sticky-header.js
+++ src/components/table/sticky-header.js
@@ -19,6 +19,7 @@ class StickyHeader extends React.PureComponent {
     React.Children.toArray(row.props.children).forEach((cell, c) => {
       cells.push(
         React.cloneElement(cell, {
+          cellRef: node => this.props.cellRef(node, c),
           key: `stickyHeader-${c}`,
         })
       );
