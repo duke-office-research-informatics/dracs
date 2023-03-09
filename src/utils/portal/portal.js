e0c1b4b4a100d04a59fc7ac0d5da07dfe70bf12d
James Helton (he/him/his) <james.helton@duke.edu>
2023-03-09 11:48:43 -0500

    Cleanup

--- src/utils/portal/portal.js
+++ src/utils/portal/portal.js
@@ -3,55 +3,15 @@ import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
 const Portal = props => {
-  const propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    container: PropTypes.oneOfType([PropTypes.node, PropTypes.object]),
-    style: PropTypes.object,
-  };
-
-  // static defaultProps = {
-  //   className: "",
-  // };
-
   const [container, setContainer] = useState(getContainer(props.container));
-  // state = {
-  //   // container: document.body,
-  //   container: document.getElementsByTagName("main")[0],
-  // };
-
-  // componentDidMount() {// I think this might definitely be what's going on
-  //   console.log("Portal (from ExpandedCard) has entered the chat")
-  //   // this.setState({ container: getContainer(props.container) });
-  // }
-
-  // componentDidUpdate(prevProps) {
-  //   console.log("Portal (from ExpandedCard) props changed")
-  //   if (prevProps.container !== props.container) {
-  //     // this.setState({ container: getContainer(props.container) });
-  //   }
-  // }
-
-  // componentWillUnmount() {
-  //   console.log("Portal (from ExpandedCard) has left the chat")
-  // }
 
   useEffect(() => {
-    console.log("Portal (from ExpandedCard) props changed");
     const newContainer = getContainer(props.container);
     if (newContainer != container) {
       setContainer(newContainer);
     }
   }, [props.container]);
 
-  useEffect(() => {
-    console.log("Portal (from ExpandedCard) has entered the chat");
-    return () => {
-      console.log("Portal (from ExpandedCard) has left the chat");
-    };
-  }, []);
-
-  console.log("Portal (from ExpandedCard) is rendering");
   /** adding the wrapping div is to make sure that legacy classes and styles
    * from previous portal implementations are applied as expected
    *
@@ -72,5 +32,12 @@ function getContainer(container) {
   const _container = typeof container === "function" ? container() : container;
   return ReactDOM.findDOMNode(_container) || document.body; //eslint-disable-line
 }
+
+Portal.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  container: PropTypes.oneOfType([PropTypes.node, PropTypes.object]),
+  style: PropTypes.object,
+};
 
 export default Portal;
