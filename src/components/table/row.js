64cbff379c9cc3c2931fd84c55e3b7c373caeb4f
James Helton (he/him/his) <james.helton@duke.edu>
2023-07-12 11:52:09 -0400

    Revert "Add SOM color palette; add child css to table row component"
    This reverts commit a2b82bd74f97167177662978198932669d6c321d.

--- src/components/table/row.js
+++ src/components/table/row.js
@@ -2,18 +2,7 @@ import React from "react";
 import propTypes from "prop-types";
 import styled from "styled-components";
 
-const RowEl = styled.tr`
-  ${p =>
-    p.childStyle?.firstChild
-      ? `&:first-child {${p.childStyle.firstChild}}`
-      : ""}
-  ${p =>
-    p.childStyle?.lastChild ? `&:last-child {${p.childStyle.lastChild}}` : ""}
-  ${p =>
-    p.childStyle?.nth
-      ? `&:nth-child${p.childStyle.nth.selector} {${p.childStyle.nth.css}}`
-      : ""}
-`;
+const RowEl = styled.tr``;
 
 class Row extends React.PureComponent {
   render() {
