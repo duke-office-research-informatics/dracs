4439f83b7e96163f61f7314148fb63d859c04835
James Helton (he/him/his) <james.helton@duke.edu>
2023-07-12 11:54:54 -0400

    Add SOM Color Palette

---
+++ src/theme/colors/somColorPalette.js
@@ -0,0 +1,49 @@
+const somColorPalette = {
+  mrhBackground: "rgba(246,245,244,1)",
+  black: "rgba(0,0,0,1)",
+  white: "rgba(255,255,255,1)",
+  /**
+   * Unsure whether these are part of the SOM palette.  Used in SOM palette in framer
+   */
+  paleHighlights: {
+    orbsPaleNeutralGray: "rgba(242,242,243,1)",
+    orbsDangerHighlight: "rgba(254,208,205,1)",
+    orbsSuccessHighlight: "rgba(236,240,193,1)",
+    orbsWarningHighlight: "rgba(254,240,205,1)",
+    orbsSkyBlueHighlight: "rgba(221,238,248,1)",
+  },
+  primary: {
+    dukeMedPrimaryDukeMedBlue: "rgba(0,83,155,1)",
+    dukeMedPrimaryDukeNavy: "rgba(0,26,87,1)",
+    dukeMedPrimaryDukeGray: "rgba(88,89,91,1)",
+  },
+  secondary: {
+    dukeMedSecondaryDukeGreen: "rgba(177,185,81,1)",
+    dukeMedSecondaryDukeDarkGreen: "rgba(78,115,42,1)",
+    dukeMedSecondaryDukeOrange: "rgba(242,133,33,1)",
+    dukeMedSecondaryDukePurple: "rgba(105,62,113,1)",
+    dukeMedSecondaryDukeRed: "rgba(220,74,56,1)",
+    dukeMedSecondaryDukeSkyBlue: "rgba(6,128,205,1)",
+    dukeMedSecondaryDukeYellow: "rgba(246,200,103,1)",
+  },
+  tertiary: {
+    dukeMedTertiaryDukeSlate: "rgba(100,118,164,1)",
+    dukeMedTertiaryDukeSlateLighter: "rgba(165,173,202,1)",
+    dukeMedTertiaryDukeRedDark: "rgba(174,62,55,1)",
+    dukeMedTertiaryDukeRedLight: "rgba(253,111,101,1)",
+    dukeMedTertiaryDukeOrangeDark: "rgba(204,102,40,1)",
+    dukeMedTertiaryDukeOrangeLight: "rgba(253,187,127,1)",
+    dukeMedTertiaryDukeYellowDark: "rgba(230,171,46,1)",
+    dukeMedTertiaryDukeYellowLight: "rgba(254,229,170,1)",
+    dukeMedTertiaryDukeGreenDark: "rgba(78,115,42,1)",
+    dukeMedTertiaryDukeGreenPale: "rgba(215,224,125,1)",
+    dukeMedTertiaryDukeSkyBlueLighter: "rgba(106,179,225,1)",
+    dukeMedTertiaryDukeSkyBluePale: "rgba(180,217,240,1)",
+    dukeMedTertiaryDukePurpleLighter: "rgba(150,122,154,1)",
+    dukeMedTertiaryDukePurplePale: "rgba(193,178,196,1)",
+    dukeMedTertiaryDukeGrayLighter: "rgba(128,130,133,1)",
+    dukeMedTertiaryDukeGrayPale: "rgba(188,190,192,1)",
+  },
+};
+
+export default somColorPalette;
