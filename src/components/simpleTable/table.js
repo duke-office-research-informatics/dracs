ca5d7e62576aa503b5ceb8009cb8e4348ead5f2c
James Helton (he/him/his) <james.helton@duke.edu>
2023-07-25 15:48:29 -0400

    cleanup

--- src/components/simpleTable/table.js
+++ src/components/simpleTable/table.js
@@ -1,14 +1,6 @@
 import React from "react";
 import propTypes from "prop-types";
 import styled from "styled-components";
-import _ from "lodash";
-import elementResizeEvent from "element-resize-event";
-
-import filterReactChildren from "../../utils/filter-react-children/filter-react-children.js";
-
-// import StickyColumn, { StickyColumnWrap } from "./sticky-column.js";
-// import StickyHeader, { StickyHeaderWrap } from "./sticky-header.js";
-// import StickyCorner, { StickyCornerWrap } from "./sticky-corner.js";
 
 import TableWrap from "../table/table-container.js";
 import TableHead from "../table/head-container.js";
@@ -48,85 +40,13 @@ const ElementWrap = styled.div`
   }
 `;
 
-const isTableHead = child => {
-  if (child.props.isTableHead) {
-    // console.log(child);
-    return true;
-  } else {
-    return false;
-  }
+const Table = ({ children, ...otherProps }) => {
+  return (
+    <ElementWrap {...otherProps}>
+      <TableWrap>{children}</TableWrap>
+    </ElementWrap>
+  );
 };
-const isTableBody = child => {
-  if (child.props.isTableBody) {
-    return true;
-  } else {
-    return false;
-  }
-};
-const isTableRow = child => {
-  if (child.props.isTableRow || child.props.isColumnAttribute) {
-    return true;
-  } else {
-    return false;
-  }
-};
-const isColumnAttribute = child => {
-  if (child.props.isColumnAttribute) {
-    return true;
-  } else {
-    return false;
-  }
-};
-class Table extends React.PureComponent {
-  static propTypes = {
-    children: propTypes.node,
-    // getRowTuples: propTypes.func,
-    // highlightRowOnHover: propTypes.bool,
-    // onChange: propTypes.func,
-    // multiSelectable: propTypes.bool,
-    // onRowClick: propTypes.bool,
-    // onRowMouseEnter: propTypes.func,
-    // onRowMouseLeave: propTypes.func,
-    // onRowSelect: propTypes.func,
-    // rowHoverColor: propTypes.string,
-    // selectable: propTypes.bool,
-    // selected: propTypes.array,
-    // resizeTimeout: propTypes.number,
-    // responsiveStickyColumn: propTypes.bool,
-    // stickyColumn: propTypes.bool,
-    // stickyColumnCount: propTypes.number,
-    // stickyHeader: propTypes.bool,
-    headerColor: propTypes.string,
-  };
-
-  renderBuiltIns = () => {
-    return React.Children.toArray(
-      filterReactChildren(this.props.children, isColumnAttribute)
-    );
-  };
-
-  renderHeader = () => {
-    return React.Children.toArray(
-      filterReactChildren(this.props.children, isTableHead)
-    );
-  };
-
-  renderBody = () => {
-    return React.Children.toArray(
-      filterReactChildren(this.props.children, isTableBody)
-    );
-  };
-
-  render() {
-    const { children, ...otherProps } = this.props;
-
-    return (
-      <ElementWrap {...otherProps}>
-        <TableWrap>{this.props.children}</TableWrap>
-      </ElementWrap>
-    );
-  }
-}
 
 export {
   Table,
